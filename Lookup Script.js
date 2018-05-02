7e850ee8e5d364dbd7a46972b728fb9f21b0115f
mehdibz <bz.mehdi@gmail.com>
2018-05-02 00:05:29 +0000

    Final

--- Lookup Script.js
+++ Lookup Script.js
@@ -1,6 +1,8 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 var input = process.argv[2];
+var moment = require('moment');
+moment().format();
 
 const client = new pg.Client({
   user     : settings.user,
@@ -16,12 +18,17 @@ client.connect((err) => {
     return console.error("Connection Error", err);
   }
   var query_arguments = [input];
-  client.query('SELECT * from famous_people WHERE first_name = $1 OR last_name = $1 LIMIT 1', query_arguments, (err, result) => {
+  client.query('SELECT * from famous_people WHERE first_name = $1 OR last_name = $1 ', query_arguments, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows); //output: 1
-    // client.end((err) => { console.log("all gone");});
+
+ 	result.rows.forEach(function (obj, index) {
+      console.log(`- ${obj.first_name} ${obj.last_name}, born '${moment(obj.birthdate).format("Y-MM-DD")}'`);
+
+  	});
+
     client.end();
   });
 });
+
